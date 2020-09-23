cf588fafaf39dc404fd35af5b964684f20670c24
俊华 蔡 <1032031912@qq.com>
2020-09-23 08:54:00 +0800

    添加项目文件。

---
+++ 9_23_多态2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_23_多态2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
