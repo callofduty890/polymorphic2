09f8bb17201f02faff01b3b8eb6068fe2d6b9dec
俊华 蔡 <1032031912@qq.com>
2020-09-23 09:47:10 +0800

    继承实现多态2

---
+++ 01_继承实现多态2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_继承实现多态2
+{
+    interface ICommon { void f(); }//声明接口，定义无返回值函数
+    abstract class Base { public abstract void g(); }//抽象类
+    class Derived1 :  Base,ICommon
+    {
+        //实现从接口继承的方法
+        public void f() { Console.WriteLine("Derived1.f()"); }
+        //重写从基类继承的方法
+        public override void g(){Console.WriteLine("Derived1.g()");}
+        //重新定义新方法h
+        public void h() { Console.WriteLine("Derived1.h()"); }
+    }
+    class Derived2 : ICommon
+    {
+        //实现从接口继承的方法
+        public void f() { Console.WriteLine("Derived2.f()"); }
+        //重新定义新方法h
+        public void h() { Console.WriteLine("Derived2.h()"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //可以调用Derived1类及其继承的所有方法
+            Derived1 d1 = new Derived1();d1.f();d1.g();d1.h();
+            Console.WriteLine("=======================");
+            //Derived1 已经实现接口故可以调用接口成员函数
+            ICommon c1 = new Derived1();c1.f();
+            //访问Derived1 中重写的方法
+            Base b1 = new Derived1();b1.g();
+            Console.WriteLine("=======================");
+            //b1.f();  //编译错误！，Base中没有声明方法
+            object o1 = new Derived1();
+            Console.WriteLine(o1.GetType());
+            //o1.f();//编译错误。object没有包含f()的方法
+            //Base b2 = new Derived2();//编译错误，没有相同的实现
+
+            Console.ReadKey();
+        }
+    }
+}
--- 9_23_多态2/Program.cs
+++ 9_23_多态2/Program.cs
@@ -26,19 +26,23 @@ namespace _9_23_多态2
     {
         static void Main(string[] args)
         {
+            //1.父类的调用：出现无法父类直接转换为子类
             Parent oparent = new Parent();
             oparent.MethodA();//OK  调用Parent的方法
             //Child ochild = (Child)oparent;//错误：子类装父类
             //ochild.MethodB();
 
+            //2.子类的调用: 功能函数正常
             Child oChild = new Child();
             oChild.MethodA();    //OK 调用基类Parent 成员方法
             oChild.MethodB();    //OK 调用派生类的方法
 
+            //3.父类中装载了子类，但是父类中没有定义子类B的方法
             Parent oparent1 = oChild;//父类装子类的对象
             oparent.MethodA(); //OK 调用派生类的方法
             //oparent.MethodB(); //编译错误！，基类不包含方法
 
+            //4.取出对应子类的对象，并由子类装载调用其方法
             Child oChild2 = (Child)oparent1;
             oChild2.MethodA();  //OK 调用基类Parent 成员方法
             oChild2.MethodB();  //OK 调用派生类的方法
